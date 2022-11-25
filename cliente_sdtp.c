931fac6d101299904c5c12a0ccd8f662b5a0798c
Joao Borges <joaoborges6@gmail.com>
2022-11-25 20:42:33 -0300

    adicionando algumas libs que faltavam

--- cliente_sdtp.c
+++ cliente_sdtp.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
